2d01465870d25fffe47e2c50f1b91319bd0e0a48
Matheus Anjos <103960040+MatheusAnjos10@users.noreply.github.com>
2022-04-25 19:18:05 -0300

    Sistema de Multas

---
+++ Aula004/ex001.js
@@ -0,0 +1,6 @@
+var vel = 61
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+if(vel > 60) {  //Condição Simples//
+    console.log('Você ultrapassou a velocidade permitida. MULTADO')
+}
+console.log('Dirija com cuidado e use sempre o cinto de segurança!!')
---
+++ Aula004/ex002.js
@@ -0,0 +1,6 @@
+var media = 2
+if (media >= 5) {
+    console.log('APROVADO')
+} else {
+    console.log('REPROVADO')
+}
